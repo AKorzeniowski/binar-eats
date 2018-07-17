ffdf7a2f98c8c748ce3528f73cb8a4361f866c62
Schocker <adikorzeniowski@gmail.com>
2018-07-17 14:45:25 +0200

    [add] Presence validations to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ApplicationRecord
+  validates :creator, :deadline, presence: true
+
+  has_one :creator, :class_name => "User"
+  has_one :orderer, :class_name => "User"
+  has_one :deliverer, :class_name => "User"
 end
