0bc2768af72c21485b7bd84a9e8ad2f5a45bec67
Schocker <adikorzeniowski@gmail.com>
2018-07-17 15:24:08 +0200

    [add] Implement new and create functions in orders_controlller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,20 @@
 class OrdersController < ApplicationController
   def new
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
+    if @post.save
+      redirect_to root, notice: 'Order was created'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:deadline)
   end
 end
