ebccc7d07da7bb76760b92fec0695d42e2a25215
PrzemekPolec <przemekp85@gmail.com>
2018-07-18 08:19:48 +0200

    changes in views

--- db/migrate/20180717110614_create_places.rb
+++ db/migrate/20180717110614_create_places.rb
@@ -1,7 +1,6 @@
 class CreatePlaces < ActiveRecord::Migration[5.2]
   def change
     create_table :places do |t|
-      t.string :id
       t.string :name
       t.string :address
       t.string :menu_url
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_17_084947) do
+ActiveRecord::Schema.define(version: 2018_07_17_110614) do
+
+  create_table "places", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "menu_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
