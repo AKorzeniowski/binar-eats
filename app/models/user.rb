5fa8f69045da5efa372f2b7b73f111fbdba1d35b
Damian Sieroń <damiansieron94@gmail.com>
2018-07-18 12:59:55 +0200

    [add] Account number - integer only

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :account_number, length: { is: 26 }, allow_blank: true
+  validates :account_number, length: { is: 26 }, allow_blank: true, numericality: { only_integer: true }
   validates :nickname, format: { with: /\A[a-zA-Z]+\z/, message: "must have only letters" }, allow_blank: true
 end
