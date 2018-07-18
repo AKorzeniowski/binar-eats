c83d2b64cc2d1d931396c70e2c8d79074fb64df1
Sebastian Andrzejczak <214877@edu.p.lodz.pl>
2018-07-18 11:59:23 +0200

    [add] Suborder view, methods new and create in controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'orders/new'
   get 'orders/create'
+  get 'orders/:id', to: 'suborders#new', as: 'order'
+  # resources :suborders
+  # get 'suborders/new'
+  # get 'suborders/create'
   root 'home#welcome'
   devise_for :users, controllers: { registrations: 'users/registrations'  }
 end
