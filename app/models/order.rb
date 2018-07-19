917081a733112fa567ea61c5466e00bd0ff6ecfa
Schocker <adikorzeniowski@gmail.com>
2018-07-19 10:11:50 +0200

    [fix] Relations for Order and Place

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,8 @@
 class Order < ApplicationRecord
   validates :creator, :deadline, presence: true
 
-  has_one :creator, :class_name => "User"
-  has_one :orderer, :class_name => "User"
-  has_one :deliverer, :class_name => "User"
+  belongs_to :creator, :class_name => "User"
+  belongs_to :orderer, :class_name => "User"
+  belongs_to :deliverer, :class_name => "User"
+  belongs_to :place
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ApplicationRecord
   validates :name, presence: true
   validates :menu_url , presence: true
+
+  has_many :orders
 end
