39ee8255f7650ce2de0f38b78cdc7d2bb8ed4f3d
Sebastian Andrzejczak <214877@edu.p.lodz.pl>
2018-07-19 13:36:00 +0200

    [fix] Small fix for variables.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
     @order = Order.find_by_id(params[:id])
-    @orderer_check = false
       redirect_to root_path, alert: "Order with id #{params[:id]} doesn't exist!" unless @order
   end
 
