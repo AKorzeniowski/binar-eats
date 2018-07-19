0664f9924ef54eb92391c5477738f8c0f10aaf04
Sebastian Andrzejczak <214877@edu.p.lodz.pl>
2018-07-19 11:59:58 +0200

    [add] Cost for item in form.
    [fix] Root path redirect for order and item.
    
    [add] Access to certain pages for logged user only.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,16 @@
 class ItemsController < ApplicationController
   def new
     @item = Item.new
+    @order = Order.find_by_id(params[:id])
+    if @order == nil
+      redirect_to root_path, alert: "Nie istenieje zamówienie o id #{params[:id]}!"
+    end
   end
 
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root, notice: 'Item was created'
+      redirect_to root_path, notice: 'Item was created'
     else
       render :new
     end
