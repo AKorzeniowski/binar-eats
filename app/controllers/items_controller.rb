003f5166af21c49b1eb263b838296fa33dfdd76b
Sebastian <41000236+AndrzejczakSebastian@users.noreply.github.com>
2018-07-19 15:31:57 +0200

    Item order (#15)
    * [add] User can order food in Item form.
    
    [add] Db seed for development purpose.
    
    [fix] Lesser small fixes.
    
    * [fix] Cost param in item fix.
    
    * [add] Setting user as orderer on demand.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,17 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
+    if params[:item][:cost] == ""
+      params[:item][:cost] = 0
+    end
+
+    if params.has_key?(:orderer) && params[:orderer] == "true"
+        ord = Order.find_by_id(params[:item][:order_id])
+        ord.orderer_id = current_user.id
+        ord.save
+    end
+
+    @item = Item.new(item_params.merge(user_id: current_user.id) )
     if @item.save
       redirect_to root_path, notice: 'Item was created'
     else
@@ -15,8 +25,7 @@ class ItemsController < ApplicationController
   end
 
   private
-
   def item_params
-    #params.require(:order).permit(:id)
+    params.require(:item).permit(:food, :cost, :order_id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,26 @@
-Place.create(name: "Pasta Go", address: "Kościuszki 32", menu_url: "https://http://pastago.pl/menu/")
-Place.create(name: "Gastromachina", address: "Piotrkowska 89", menu_url: "https://www.facebook.com/gastromachina/menu/")
-Place.create(name: "Zahir", address: "6 Sierpnia", menu_url: "http://zahirkebab.pl/lokale/lodz-ul-6-go-sierpnia-13-centrum/")
-Place.create(name: "Da Grasso", address: "Zachodnia 105", menu_url: "https://www.dagrasso.pl/lodz-zachodnia")
+def create_places
+  @place1 = Place.create(name: "Pasta Go", address: "Kościuszki 32", menu_url: "https://http://pastago.pl/menu/")
+  @place2 = Place.create(name: "Gastromachina", address: "Piotrkowska 89", menu_url: "https://www.facebook.com/gastromachina/menu/")
+  @place3 = Place.create(name: "Zahir", address: "6 Sierpnia", menu_url: "http://zahirkebab.pl/lokale/lodz-ul-6-go-sierpnia-13-centrum/")
+  @place4 = Place.create(name: "Da Grasso", address: "Zachodnia 105", menu_url: "https://www.dagrasso.pl/lodz-zachodnia")
+end
+
+def reset_database()
+  Item.destroy_all
+  Order.destroy_all
+  User.destroy_all
+  Place.destroy_all
+end
+
+if Rails.env == "development"
+  reset_database
+  create_places
+  @user1 = User.create(email: "one@user.pl", password: "123456")
+  @user2 = User.create(email: "two@two.pl", password: "123456")
+  @user3 = User.create(email: "three@two.pl", password: "123456")
+  Order.create(creator_id: @user1.id, place_id: @place1.id, deadline: 2.hours.from_now)
+  Order.create(creator_id: @user2.id, place_id: @place2.id, deadline: 5.hours.from_now, orderer_id: @user2.id)
+  Order.create(creator_id: @user3.id, place_id: @place3.id, deadline: 3.hours.from_now, orderer_id: @user1.id, deliverer_id: @user2.id)
+else
+  create_places
+end
