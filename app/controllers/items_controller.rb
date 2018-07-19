e82799580b3c5b577cd7eaadf17c083d3e15053e
Sebastian Andrzejczak <214877@edu.p.lodz.pl>
2018-07-19 15:25:18 +0200

    [add] Setting user as orderer on demand.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,16 @@ class ItemsController < ApplicationController
   end
 
   def create
+    if params[:item][:cost] == ""
+      params[:item][:cost] = 0
+    end
+
+    if params.has_key?(:orderer) && params[:orderer] == "true"
+        ord = Order.find_by_id(params[:item][:order_id])
+        ord.orderer_id = current_user.id
+        ord.save
+    end
+
     @item = Item.new(item_params.merge(user_id: current_user.id) )
     if @item.save
       redirect_to root_path, notice: 'Item was created'
@@ -16,9 +26,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    if params[:item][:cost] == ""
-      params[:item][:cost] = 0
-    end
-    params.require(:item).permit(:food, :cost, :order_id, :orderer)
+    params.require(:item).permit(:food, :cost, :order_id)
   end
 end
