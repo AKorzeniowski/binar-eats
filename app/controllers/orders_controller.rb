2a13a301ff213f028caeacd6f92b4e69b232aacd
Sieron94PL <damiansieron94@gmail.com>
2018-07-19 23:26:20 +0200

    Order view (#14)
    * [add] Order model test
    
    * [add][fix] Order tests: add validations, fix problem with relations.
    
    * Resolve conflict
    
    * [add] Add Order view
    
    * [fix] Labels for deliverer

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,14 @@ class OrdersController < ApplicationController
     @order = Order.new
   end
 
+
   def create
+    if params[:order][:orderer_id].to_i == -1
+      params[:order][:orderer_id] = nil
+    end
+    if params[:order][:deliverer_id].to_i == -1
+      params[:order][:deliverer_id] = nil
+    end
     @order = Order.new(order_params)
     if @order.save
       redirect_to root_path, notice: 'Order was created'
@@ -15,6 +22,6 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:deadline)
+    params.require(:order).permit(:creator_id, :place_id, :orderer_id, :deliverer_id, :deadline)
   end
 end
