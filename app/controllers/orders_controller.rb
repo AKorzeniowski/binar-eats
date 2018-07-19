24c5e16271e9d6810d9c4b91b3b7bb20e165639b
Damian Sieroń <damiansieron94@gmail.com>
2018-07-19 13:05:33 +0200

    Resolve conflict

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,6 +15,11 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:deadline)
+    # puts DateTime.new(params[:order]['deadline(1)'].to_i, params[:order]['deadline(2)'].to_i,
+    #              params[:order]['deadline(3)'].to_i, params[:order]['deadline(4)'].to_i,
+    #              params[:order]['deadline(5)'].to_i)
+    # puts DateTime.new(DateTime.now.year, DateTime.now.month, DateTime.now.day, params[:order]['deadline(4)'].to_i, params[:order]['deadline(5)'].to_i, 0)
+    params.require(:order).permit(:place_id, :orderer_id, :deliverer_id)
+    # puts 'coś tam' + params[:order][:deadline].to_s
   end
 end
