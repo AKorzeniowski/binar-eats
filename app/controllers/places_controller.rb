032688dc86ad968f41342c8bdabf86d37745ba40
PrzemekPolec <przemekp85@gmail.com>
2018-07-20 11:32:30 +0200

    create extra place menu

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,12 @@
 class PlacesController < ApplicationController
   def index
     @places = Place.all
+    @place = Place.new
   end
+  def create
+    @place = Place.new(params[:id])
+    @place.save
+    redirect_to places_path, notice: 'New place was created'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations'  }
 
   authenticate :user do
-    get 'places/index'
+    resources :places, only:[:index, :create]
     resources :items, only:[:new, :create]
     resources :orders, only:[:new, :create]
     get 'orders/:id', to: 'items#new', as: 'order'
   end
+
 end
