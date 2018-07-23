22a07eec07966ee98c2a275ccf6b0b06b0077040
Sebastian <41000236+AndrzejczakSebastian@users.noreply.github.com>
2018-07-23 10:05:06 +0200

    Item cost test (#25)
    * [fix] Relations model changes fix.
    
    * [add] Test for presence of cost.

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -6,11 +6,12 @@ RSpec.describe Item, type: :model do
   describe 'validates' do
     it { should validate_presence_of(:user) }
     it { should validate_presence_of(:order) }
+    it { should validate_presence_of(:cost) }
   end
 
   describe 'relations' do
-    it { should have_one(:user) }
-    it { should have_one(:order) }
+    it { should belong_to(:user) }
+    it { should belong_to(:order) }
   end
 
 end
