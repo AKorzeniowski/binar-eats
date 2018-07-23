9d25a5a9b6d9bc86ea308dc04f6ed0ca5fec8d89
PrzemekPolec <41000327+PrzemekPolec@users.noreply.github.com>
2018-07-23 14:52:13 +0200

    Places admin (#34)
    * create model Place
    
    model has id, name,address, menu_url
    
    * create validates
    
    * generate controller for Places
    
    * changes in views
    
    * create Place options
    
    * create adding place to order
    
    * create seed.db, add places
    
    * fix some syntax problem
    
    * add extra place to places
    
    * add new place form to places
    
    * create extra place menu
    
    * add new function- adding new place to places list
    
    * add new function to places
    
    add new function to places: edit and destroy, add test to places
    
    * fix resources for places
    
    * fix all identation, grammar mistakes and syntax error

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,39 @@
 class PlacesController < ApplicationController
   def index
     @places = Place.all
+    @place = Place.new
+  end
+  def create
+    @place = Place.new(place_params)
+    if @place.save
+       redirect_to places_path, notice: 'New place was created'
+    else
+       redirect_to places_path, alert: 'New place was not created'
+    end
   end
 
+  def edit
+    @place = Place.find(params[:id])
+  end
 
+  def update
+    @place = Place.find(params[:id])
+    if @place.update_attributes(place_params)
+        flash[:notice] = "Place updated."
+        redirect_to places_path
+    else
+        render action: 'edit'
+    end
+  end
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    flash[:notice] = "Place deleted"
+    redirect_to places_path
+  end
+
+   private
+   def place_params
+     params.require(:place).permit(:name, :menu_url)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,15 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations'  }
 
   authenticate :user do
+
     get 'places/index'
+    resources :places
     resources :items, only:[:new, :create, :show, :update, :destroy]
     resources :orders, only:[:new, :create, :index, :edit, :update]
     get 'orders/:id', to: 'items#new'
     get 'order/:id/items', to: 'orders#items'
 
+
   end
+
 end
