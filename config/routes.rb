e9e91794a3c6678fa0af1339486af23764570fb8
PrzemekPolec <przemekp85@gmail.com>
2018-07-23 14:23:13 +0200

    fix resources for places

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   authenticate :user do
 
     get 'places/index'
+    resources :places#,only:[:index, :create, :edit, :update, :dele
     resources :items, only:[:new, :create, :show, :update, :destroy]
     resources :orders, only:[:new, :create, :index, :edit, :update]
     get 'orders/:id', to: 'items#new'
