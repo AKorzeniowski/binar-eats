68cf77dd1d26cb78c225ee024189db9d5078b1bf
PrzemekPolec <przemekp85@gmail.com>
2018-07-23 10:26:35 +0200

    add new function- adding new place to places list

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,9 +4,36 @@ class PlacesController < ApplicationController
     @place = Place.new
   end
   def create
-    @place = Place.new(params[:id])
-    @place.save
-    redirect_to places_path, notice: 'New place was created'
+    @place = Place.new(place_params)
+    if @place.save
+       redirect_to places_path, notice: 'New place was created'
+    else
+         redirect_to places_path, alert: 'New place was not created'
+    end
   end
 
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    if @place.update_attributes(place_params)
+        flash[:notice] = "Place updated."
+        redirect_to places_path
+    else
+      render action: 'edit'
+    end
+  end
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    flash[:notice] = "Place deleted"
+    redirect_to places_path
+  end
+
+   private
+   def place_params
+     params.require(:place).permit(:name, :menu_url)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations'  }
 
   authenticate :user do
-    resources :places, only:[:index, :create]
+    resources :places#,only:[:index, :create, :edit, :update, :delete]
     resources :items, only:[:new, :create]
     resources :orders, only:[:new, :create]
     get 'orders/:id', to: 'items#new', as: 'order'
