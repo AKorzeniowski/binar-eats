25fa6a4243bca2609ae08217509755d92b92696c
Damian Sieroń <damiansieron94@gmail.com>
2018-07-23 12:04:11 +0200

    [fix] Add missing relations tests to Order model.

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,20 +3,22 @@ require 'rails_helper'
 RSpec.describe Order, type: :model do
   describe 'attributes' do
     it 'should have extra attributes' do
-      expect(subject.attributes).to include('deadline', 'delivery_cost', 'creator_id', 'orderer_id', 'deliverer_id')
+      expect(subject.attributes).to include('deadline', 'delivery_cost', 'creator_id', 'orderer_id', 'deliverer_id', 'place_id')
     end
   end
 
   describe 'validations' do
     it { should validate_presence_of(:creator) }
     it { should validate_presence_of(:deadline) }
+    it { should validate_presence_of(:place) }
   end
 
 
   describe 'relations' do
-    it { have_one(:creator) }
-    it { have_one(:orderer) }
-    it { have_one(:deliverer) }
+    it { should belong_to(:creator) }
+    it { should belong_to(:orderer) }
+    it { should belong_to(:deliverer) }
+    it { should belong_to(:place) }
   end
 
 end
