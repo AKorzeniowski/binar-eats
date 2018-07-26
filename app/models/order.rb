8129a69a2772fbd01c910a964c728a899392a3a6
Sieron94PL <damiansieron94@gmail.com>
2018-07-26 11:15:47 +0200

    Order (#56)
    * [conf] Configuration devise for tests
    
    * Order controller tests
    
    * [refactor] Methods from models scopes in: index, items
    [add] Add scopes to Item model: creator_order_items, other_order_items
    [add] Add scopes to Order model: my_orders, other_orders
    [add] Test for items in Order controller
    [add] Add test relation has many: order -> items in Order model
    
    * [fix] rubocop

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,11 @@ class Order < ApplicationRecord
   belongs_to :orderer, class_name: 'User', optional: true
   belongs_to :deliverer, class_name: 'User', optional: true
   belongs_to :place
+  has_many :items, dependent: :destroy
+
+  scope :my_orders, ->(creator_id) { where('creator_id = ? AND DATE(deadline) = ?', creator_id, Time.zone.today) }
+  scope :other_orders, ->(creator_id) {
+    where.not('creator_id = ?', creator_id).
+      where('DATE(deadline) = ?', Time.zone.today)
+  }
 end
