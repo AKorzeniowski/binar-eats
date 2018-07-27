6267498208917b7976d134295a73eb5efb5fbb76
Adrian Korzeniowski <adikorzeniowski@gmail.com>
2018-07-27 13:27:58 +0200

    [fix] Move JS to assests

---
+++ app/assets/javascripts/order-wizard.js
@@ -0,0 +1,26 @@
+$(document).on('turbolinks:load', function() {
+	const steps = document.getElementsByClassName('wizard__step')
+
+	for (let step of steps) { hide(step) }
+
+	var currentStep = 0
+	show(steps[currentStep])
+
+	const nextButtons = document.getElementsByClassName('wizard__button--next')
+	for (let button of nextButtons) {
+		button.onclick = (event) => {
+			event.preventDefault()
+			hide(steps[currentStep])
+			currentStep++
+			show(steps[currentStep])
+		}
+	}
+
+	function show(element) {
+		element.style.display = 'block'
+	}
+
+	function hide(element) {
+		element.style.display = 'none'
+	}
+});
