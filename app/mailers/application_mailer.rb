79ed9e1987385e474200def2176106c1e1c97cb0
Sebastian <214877@edu.p.lodz.pl>
2018-07-27 15:31:31 +0200

    [add] Mailer action for sending payoff mails.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,14 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  def payoff_mail
+    @order = params[:order]
+
+    @order.items.where(has_paid: nil).each do |item|
+      @url  = url_for(action: "payoff", id:item.id, controller: 'items', only_path: false)
+      @item = item
+      mail(to: item.user.email, subject: "Payoff for your item from #{@order.place.name}.")
+    end
+  end
 end
