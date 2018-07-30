ac3fa77b5579fce5ff326dd776dc13625ac35a7f
Adrian Korzeniowski <adikorzeniowski@gmail.com>
2018-07-30 13:18:04 +0200

    [add] Back button in order creation form

--- app/assets/javascripts/order-wizard.js
+++ app/assets/javascripts/order-wizard.js
@@ -7,13 +7,21 @@ $(document).on('turbolinks:load', function() {
 	show(steps[currentStep])
 
 	const nextButtons = document.getElementsByClassName('wizard__button--next')
+
 	for (let button of nextButtons) {
-		button.onclick = (event) => {
-			event.preventDefault()
-			hide(steps[currentStep])
-			currentStep++
-			show(steps[currentStep])
-		}
+		button.onclick = (event) => { changeStep(event, 1) }
+	}
+
+	const prevButtons = document.getElementsByClassName('wizard__button--prev')
+	for (let button of prevButtons) {
+		button.onclick = (event) => { changeStep(event, -1) }
+	}
+
+	function changeStep(event, amount) {
+		event.preventDefault()
+		hide(steps[currentStep])
+		currentStep = currentStep + amount
+		show(steps[currentStep])
 	}
 
 	function show(element) {
