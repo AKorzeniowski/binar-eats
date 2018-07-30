f4410de3daa60c738f4a1795c1103185af8e13f2
Damian Sieroń <damiansieron94@gmail.com>
2018-07-30 13:15:55 +0200

    [add] Allowing/blocking next action in case of own place order inputs.

--- app/assets/javascripts/order-wizard.js
+++ app/assets/javascripts/order-wizard.js
@@ -1,7 +1,6 @@
 $(document).on('turbolinks:load', function() {
   const steps = document.getElementsByClassName('wizard__step')
 
-
   for (let step of steps) {
     hide(step)
   }
@@ -31,12 +30,36 @@ $(document).on('turbolinks:load', function() {
     $('#order_place_id').append('<option value="5">Another place</option>')
   }
 
+  function checkInputs() {
+    if ($("input[name='own_place_name']").val().length != 0 && $("input[name='own_place_menu_url']").val().length != 0) {
+      $('.wizard__button--next').attr("disabled", false)
+    } else {
+      $('.wizard__button--next').attr("disabled", true)
+    }
+  }
+
+  function addInputs() {
+    $('#own_place').append("<tr><td>Name</td><td><input name='own_place_name' placeholder='Order place name...' required></td></tr>")
+    $('#own_place').append("<tr><td>Address</td><td><input name='own_place_address' placeholder='Order place address...'></td></tr>")
+    $('#own_place').append("<tr><td>Menu[url]</td><td><input name='own_place_menu_url' placeholder='Order place menu url...' required></td></tr>")
+  }
+
   $('#order_place_id').change(function() {
     $('#own_place').empty()
+    $('.wizard__button--next').attr("disabled", true)
     if ($(this).val() == 5) {
-      $('#own_place').append("Place name <input name='own_place_name' placeholder='Enter your order place name...' required> </br>")
-			$('#own_place').append("Place menu url <input name='own_place_menu_url' placeholder='Enter your order place menu url...' required>")
+      addInputs();
+      $("input[name='own_place_name']").change(function() {
+        checkInputs()
+      })
+      $("input[name='own_place_menu_url']").change(function() {
+        checkInputs()
+      })
+    } else {
+      $('.wizard__button--next').attr("disabled", false)
     }
   })
+
+
 
 });
