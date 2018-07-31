05f975286472e8d21474f5d9108491aca4030cf8
Adrian Korzeniowski <adikorzeniowski@gmail.com>
2018-07-31 11:14:18 +0200

    [add] JavaScript handling the button for link copying

--- app/assets/javascripts/order-link-copy.js
+++ app/assets/javascripts/order-link-copy.js
@@ -1,5 +1,9 @@
-function copyLink() {
-	var copyText = document.getElementById("order-link");
-	copyText.select();
-	document.execCommand("copy");
-}
+$(document).on('turbolinks:load', function() {
+	let copyTextButton = document.getElementById("copy-link-button")
+
+	copyTextButton.onclick = function(event) {
+		linkTextfield = document.getElementById("copy-link-textarea")
+		linkTextfield.select()
+		document.execCommand("copy")
+	}
+})
