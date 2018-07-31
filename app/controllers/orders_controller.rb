bece4570edf4a675478ab63b155ecb85bacba6ea
Sebastian <214877@edu.p.lodz.pl>
2018-07-31 13:52:35 +0200

    [fix] Permission to see payment page.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -53,9 +53,7 @@ class OrdersController < ApplicationController
   def payment
     @order = Order.find(params[:id])
 
-    return redirect_to root_path, alert: "You dont't have permission to see this page." unless
-    (current_user.id == @order.orderer_id && @order.deliverer_id) ||
-    (current_user.id == @order.orderer_id)
+    return redirect_to root_path, alert: "You dont't have permission to see this page." unless @order.allowed_to_see_payment?(current_user)
   end
 
   def done
