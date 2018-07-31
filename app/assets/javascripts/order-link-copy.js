ee4580491158bedddaf23f05609213fda2aab6cd
Adrian Korzeniowski <adikorzeniowski@gmail.com>
2018-07-31 11:44:34 +0200

    [fix] Add missing div and condition for JS

--- app/assets/javascripts/order-link-copy.js
+++ app/assets/javascripts/order-link-copy.js
@@ -1,9 +1,11 @@
 $(document).on('turbolinks:load', function() {
 	let copyTextButton = document.getElementById("copy-link-button")
 
-	copyTextButton.onclick = function(event) {
-		linkTextfield = document.getElementById("copy-link-textarea")
-		linkTextfield.select()
-		document.execCommand("copy")
+	if (copyTextButton) {
+		copyTextButton.onclick = function(event) {
+			linkTextfield = document.getElementById("copy-link-textarea")
+			linkTextfield.select()
+			document.execCommand("copy")
+		}
 	}
 })
