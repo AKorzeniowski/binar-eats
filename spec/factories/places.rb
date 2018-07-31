b6b503f87aec7514086988bf2cab090116704f42
Damian Sieroń <damiansieron94@gmail.com>
2018-07-31 12:15:06 +0200

    [add] Tests for PlacesController.

--- spec/factories/places.rb
+++ spec/factories/places.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
+
   factory :place do
-    name Faker::Restaurant.name
+    name Faker::Pokemon.name
     address Faker::Address.street_address
     menu_url Faker::Internet.url
   end
+
 end
