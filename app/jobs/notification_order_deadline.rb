e4eddccdcef4ad084abd3bb4e0fe48cb26568838
Damian Sieroń <damiansieron94@gmail.com>
2018-08-01 13:26:20 +0200

    [fix] Rubocop 1 offense.

--- app/jobs/notification_order_deadline.rb
+++ app/jobs/notification_order_deadline.rb
@@ -5,7 +5,7 @@ class NotificationOrderDeadline < ApplicationJob
     slack = SlackNotificationService.new
     message = "Deadline for Order##{order_id} will pass for 5 minutes."
     order = Order.find(order_id)
-    order.orderer_id = order.creator_id if order.orderer_id == nil
+    order.orderer_id = order.creator_id if order.orderer_id.nil?
     slack.call(User.find(order.orderer_id).email, message)
   end
 end
