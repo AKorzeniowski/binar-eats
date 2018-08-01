0cdeef53b02de69d3e1d6d3f4989277eeccefbe2
PrzemekPolec <przemekp85@gmail.com>
2018-08-01 12:17:47 +0200

    fix pending migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_30_094908) do
+ActiveRecord::Schema.define(version: 2018_08_01_100246) do
 
   create_table "items", force: :cascade do |t|
     t.integer "user_id"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2018_07_30_094908) do
     t.string "account_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
